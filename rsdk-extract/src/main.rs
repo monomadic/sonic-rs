6c33d2ca53d071b167b54c0dd72eedb37b525658
Monomadic <deathdisco@protonmail.com>
2021-12-09 23:20:00 +0800

    args

--- rsdk-extract/src/main.rs
+++ rsdk-extract/src/main.rs
@@ -1,3 +1,15 @@
+use std::env;
+
 fn main() {
     println!("RSDK Extractor");
+
+    let args: Vec<String> = env::args().collect();
+
+    if let Some(filename) = args.get(1) {
+        println!("Reading {}", filename);
+    } else {
+        println!("Usage: rsdk-extract <filename>");
+    }
+
+    println!("done.");
 }
