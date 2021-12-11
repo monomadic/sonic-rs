898ecccd55561196c79e68db7932938fe2ff5bda
Monomadic <deathdisco@protonmail.com>
2021-12-11 21:23:01 +0800

    writes files (not working yet)

--- rsdk-extract/src/extract.rs
+++ rsdk-extract/src/extract.rs
@@ -17,6 +17,7 @@ pub fn load<P: AsRef<Path>>(path: P) -> ExtractResult<()> {
 
 /** Read header data (8 bytes) */
 fn header(mut buffer: &[u8]) {
+    let mut whole_file = &buffer.clone();
     let mut magic_number = [0; 4];
     buffer.read_exact(&mut magic_number).unwrap();
     println!(
@@ -31,8 +32,24 @@ fn header(mut buffer: &[u8]) {
 
     println!("File count: {}", file_count);
 
-    for _ in 0..1 {
-        read_index(&mut buffer);
+    for index in 0..file_count {
+        // read_index(&mut buffer);
+        let mut checksumbuffer = [0; 16];
+        let checksum = buffer.read_exact(&mut checksumbuffer).unwrap();
+        let offset = buffer.read_u32::<LittleEndian>().unwrap();
+        let filesize_encoded = buffer.read_u32::<LittleEndian>().unwrap();
+        let encrypted: bool = (filesize_encoded & 0x80000000) == 0x80000000;
+        let filesize = filesize_encoded & 0x7FFFFFFF;
+        println!(
+            "Checksum: {:?}\nOffset: {}\nSize: {}\nEncrypted: {}",
+            checksum, offset, filesize, encrypted
+        );
+
+        // write stupid file
+        let file = &whole_file[(offset as usize)..((filesize + offset) as usize)];
+        use std::io::Write;
+        let mut file_buffer = std::fs::File::create(format!("output/{}.gif", index)).unwrap();
+        file_buffer.write_all(&file).unwrap();
     }
 
     // LittleEndian::read_u16(load_part(2)) as usize;
