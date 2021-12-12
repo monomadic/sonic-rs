906ce56620b64d791ed166e7b1e76ec4ae4448ab
Monomadic <deathdisco@protonmail.com>
2021-12-13 05:20:38 +0800

    generating eload keys

--- rsdk-extract/src/extract.rs
+++ rsdk-extract/src/extract.rs
@@ -20,20 +20,16 @@ fn header(mut buffer: &[u8]) {
     let dictionary = crate::dictionary::generate();
 
     let whole_file = &buffer.clone();
-    let mut magic_number = [0; 4];
+    let mut magic_number = [0; 6];
     buffer.read_exact(&mut magic_number).unwrap();
-    println!(
-        "Magic Number: {}",
-        std::str::from_utf8(&magic_number).unwrap()
-    );
+    let magic_number = std::str::from_utf8(&magic_number).unwrap();
+    println!("Magic Number: {}", magic_number);
 
-    let mut version = [0; 2];
-    buffer.read_exact(&mut version).unwrap();
-    println!("Version: {}", std::str::from_utf8(&version).unwrap());
-
-    match version[1] {
-        66 => println!("RSDKv4 Detected (Sonic the Hedgehog 1 and Sonic the Hedgehog 2 remasters)"),
-        _ => println!("Unknown or unsupported RSDK version."),
+    match magic_number {
+        "RSDKvB" => {
+            println!("RSDKvB Detected (Sonic 1 and Sonic 2 Mobile Remakes)")
+        }
+        _ => panic!("Unknown or unsupported RSDK version."),
     }
 
     let file_count = buffer.read_u16::<LittleEndian>().unwrap();
@@ -57,10 +53,10 @@ fn header(mut buffer: &[u8]) {
         let filesize_encoded = buffer.read_u32::<LittleEndian>().unwrap();
         let encrypted: bool = (filesize_encoded & 0x80000000) == 0x80000000;
         let filesize = filesize_encoded & 0x7FFFFFFF;
-        // println!(
-        //     "Checksum: {} Offset: {} Size: {} Encrypted: {}",
-        //     md5sum, offset, filesize, encrypted
-        // );
+        println!(
+            "Checksum: {} Offset: {} Size: {} Encrypted: {}",
+            md5sum, offset, filesize, encrypted
+        );
 
         // write stupid file
         let file = &whole_file[(offset as usize)..((filesize + offset) as usize)];
@@ -84,6 +80,8 @@ fn header(mut buffer: &[u8]) {
     }
 
     // LittleEndian::read_u16(load_part(2)) as usize;
+
+    generate_eload_keys(4388);
 }
 
 // /** Read a file index record (20 bytes) () */
@@ -100,3 +98,46 @@ fn header(mut buffer: &[u8]) {
 //         checksum, offset, filesize, encrypted
 //     );
 // }
+
+static ENC_KEY_2: u32 = 0x24924925;
+static ENC_KEY_1: u32 = 0xAAAAAAAB;
+
+fn decrypt(bytes: &[u8]) -> Vec<u8> {
+    for byte in bytes {}
+
+    Vec::new()
+}
+
+fn generate_eload_keys(filesize: u32) -> (String, String) {
+    let arg1 = filesize;
+    let arg2 = (filesize >> 1) + 1;
+
+    println!("using args {} {}", arg1, arg2);
+
+    // convert filesize to a string
+    // let filesize_string = filesize.to_string();
+    // let length = filesize_string.len();
+
+    // let md5_of_arg1 = md5::compute(arg1.to_string());
+
+    (generate_key(arg1), generate_key(arg2))
+}
+
+fn generate_key(i: u32) -> String {
+    let checksum = md5::compute(i.to_string());
+
+    use std::io::Cursor;
+    let mut cursor = Cursor::new(checksum.to_vec());
+
+    let key: String = [
+        cursor.read_u32::<LittleEndian>().unwrap(),
+        cursor.read_u32::<LittleEndian>().unwrap(),
+        cursor.read_u32::<LittleEndian>().unwrap(),
+        cursor.read_u32::<LittleEndian>().unwrap(),
+    ]
+    .iter()
+    .map(|h| format!("{:08x}", h))
+    .collect::<String>();
+
+    format!("{}", key)
+}
