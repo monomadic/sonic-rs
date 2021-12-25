89b4c9b233692dc0a7fb347331fb2bc44dc48b5e
Monomadic <deathdisco@protonmail.com>
2021-12-26 00:03:38 +0800

    rgba data from gifs displaying correctly

--- src/display.rs
+++ src/display.rs
@@ -1,28 +1,35 @@
 use crate::engine::Game;
-use crate::image::Image;
+// use crate::image::Image;
+use image::io::Reader as ImageReader;
+use image::*;
 use minifb::{Key, Window, WindowOptions};
 
 pub(crate) fn run(mut game: Game) -> Result<(), Box<dyn std::error::Error>> {
-    let sprite = Image::load_gif("resources/sonic/Data/Sprites/Title/Title.gif")?;
+    // let sprite = Image::load_gif("resources/sonic/Data/Sprites/Title/Title.gif")?;
 
-    println!("{} x {}", sprite.width, sprite.height);
+    let image = ImageReader::open("resources/sonic/Data/Sprites/Title/Title.gif")?.decode()?;
+    let (width, height) = image.dimensions();
+
+    println!("{} x {}", width, height);
 
     let mut window = Window::new(
         &game.config.window_title,
-        sprite.width as usize,
-        sprite.height as usize,
+        width as usize,
+        height as usize,
         WindowOptions::default(),
     )?;
 
     // Limit to max ~60 fps update rate
     window.limit_update_rate(Some(std::time::Duration::from_micros(16600)));
 
+    let u32_buffer: Vec<u32> = image
+        .into_rgb8()
+        .chunks(3)
+        .map(|v| ((v[0] as u32) << 16) | ((v[1] as u32) << 8) | v[2] as u32)
+        .collect();
+
     while window.is_open() && !window.is_key_down(Key::Escape) && !window.is_key_down(Key::Q) {
-        window.update_with_buffer(
-            &sprite.buffer_u32(),
-            sprite.width as usize,
-            sprite.height as usize,
-        )?;
+        window.update_with_buffer(&u32_buffer, width as usize, height as usize)?;
     }
 
     Ok(())
--- src/image.rs
+++ src/image.rs
@@ -9,6 +9,7 @@ impl Image {
         let file = std::fs::File::open(path)?;
         let mut decoder = {
             let mut options = gif::DecodeOptions::new();
+            // options.set_color_output(gif::ColorOutput::RGBA);
             options.allow_unknown_blocks(true);
             options.read_info(file).unwrap()
         };
@@ -23,7 +24,13 @@ impl Image {
         })
     }
 
+    // pub(crate) fn get_sprite(&self, offset_x: u32, offset_y: u32, width: u32, height: u32);
+
     pub(crate) fn buffer_u32(&self) -> Vec<u32> {
+        // let rgba = ((redbyte as u32) << 24)
+        //     | ((greenbyte as u32) << 16)
+        //     | ((bluebyte as u32) << 8)
+        //     | (alphabyte as u32);
         self.buffer
             .iter()
             .map(|v| u32::try_from(*v).unwrap())
