9281e92b734c3d455b5e39ca5ef3914dabeae823
Monomadic <deathdisco@protonmail.com>
2021-12-26 10:03:31 +0800

    1-bit transparency instead of alpha blending (might revert)

--- src/display.rs
+++ src/display.rs
@@ -6,24 +6,24 @@ use minifb::{Key, Window, WindowOptions};
 
 pub(crate) fn run(mut game: Game) -> Result<(), Box<dyn std::error::Error>> {
     let (width, height) = (320, 240);
-    let mut screen = image::DynamicImage::new_rgba8(width, height);
+    let mut screen = image::DynamicImage::new_rgb8(width, height); // use rgba8 for alpha blending
     screen.invert();
 
     // let logo = SegaLogo::new();
 
     let mut sprites = io::Reader::open("resources/sonic/Data/Sprites/Title/Title.gif")?.decode()?;
 
-    // gifs have 1-bit magenta transparency
-    if let Some(pixels) = sprites.as_mut_rgba8() {
-        for pixel in pixels.pixels_mut() {
-            if pixel[0] == 255 && pixel[1] == 0 && pixel[2] == 255 {
-                pixel[0] = 0;
-                pixel[1] = 255;
-                pixel[2] = 255;
-                pixel[3] = 0;
-            }
-        }
-    };
+    // // gifs have 1-bit magenta transparency
+    // if let Some(pixels) = sprites.as_mut_rgba8() {
+    //     for pixel in pixels.pixels_mut() {
+    //         if pixel[0] == 255 && pixel[1] == 0 && pixel[2] == 255 {
+    //             pixel[0] = 0;
+    //             pixel[1] = 255;
+    //             pixel[2] = 255;
+    //             pixel[3] = 0;
+    //         }
+    //     }
+    // };
 
     let sega_title = sprites.crop_imm(323, 242, 188, 58);
     // screen.copy_from(&sega_title, 0, 0)?;
@@ -31,7 +31,13 @@ pub(crate) fn run(mut game: Game) -> Result<(), Box<dyn std::error::Error>> {
     for k in 0..sega_title.height() {
         for i in 0..sega_title.width() {
             let p = sega_title.get_pixel(i, k);
-            screen.blend_pixel(i + 0, k + 0, p);
+            // if magenta (1-bit transparent pixel) don't draw
+            if p[0] == 255 && p[1] == 0 && p[2] == 255 {
+                // alpha blending
+                // screen.blend_pixel(i + 0, k + 0, p);
+            } else {
+                screen.put_pixel(i + 0, k + 0, p);
+            }
         }
     }
 
